eeb4b9a6dec829336b99bbb8ab514f7d7010d040
Aswanth Alakkadan <aswanth682@gmail.com>
2023-09-01 00:42:37 +0530

    user address add and delete

--- model/user.js
+++ model/user.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+    type: String,
+    phone: String,
+    houseName: String,
+    name: String,
+    street: String,
+    city: String,
+    state: String,
+    pinCode: String
+});
+
 const userSchema = new mongoose.Schema({
     username : String,
     password : String,
     phoneNumber : String,
-    address : {
-        type : String,
-        phone : String,
-        houseName : String,
-        name : String, 
-        street: String,
-        city: String,
-        state: String,
-        pinCode: String, 
-    },
+    addresses: [addressSchema],
     email : String,
     verified: Boolean,
     profileImage : String
--- routes/user routes/userhome.js
+++ routes/user routes/userhome.js
@@ -56,8 +56,90 @@ router.post('/updateProfile/:userId', upload.single('profileImage'), async (req,
     }
   });
 
-  router.get('/manageaddress',(req, res)=>{
-    res.render('./user/address')
+  router.get('/manageaddress/:userId',async(req, res)=>{
+    const userId = req.params.userId;
+    try{
+        const user = await User.findById(userId);
+        if(!user){
+            console.log("no user found");
+        }
+        res.render('./user/address', { user });
+    }catch(error){
+        console.log(error);
+    }
   })
+
+  router.get('/addaddress/:userId',async(req,res)=>{
+    const userId = req.params.userId;
+    try{
+        const user = await User.findById(userId);
+        if(!user){
+            console.log("no user found");
+        }
+        res.render('./user/address-form',{user})
+    }catch(error){
+        console.log(error);
+    }
+  })
+
+  router.post('/addAddress/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const type = req.body.type; 
+    const { phone, houseName, name, street, city, state, pinCode } = req.body;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ errorMessage: 'User not found' });
+        }
+
+        if (user.addresses.length >= 5) {
+            return res.status(400).json({ errorMessage: 'Maximum address limit reached' });
+        }
+
+        user.addresses.push({
+            type,
+            phone,
+            houseName,
+            name,
+            street,
+            city,
+            state,
+            pinCode
+        });
+
+        await user.save();
+        res.redirect('/manageaddress/' + userId)
+    } catch (error) {
+        console.error('Error adding address:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+router.post('/removeAddress/:userId/:addressIndex', async (req, res) => {
+    const userId = req.params.userId;
+    const addressIndex = req.params.addressIndex;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ errorMessage: 'User not found' });
+        }
+
+        if (addressIndex < 0 || addressIndex >= user.addresses.length) {
+            return res.status(400).json({ errorMessage: 'Invalid address index' });
+        }
+
+        user.addresses.splice(addressIndex, 1);
+        await user.save();
+        res.redirect('/manageaddress/' + userId)
+    } catch (error) {
+        console.error('Error removing address:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 
 module.exports = router
